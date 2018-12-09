22dcb581eaa13e53c1eda8f695d2f8aef5ca98fc
Iasonas Paraskevopoulos <iaswnparaskev@gmail.com>
2018-12-09 21:33:09 +0200

    Add comments to constants header.

--- headers/constants.h
+++ headers/constants.h
@@ -1,21 +1,22 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
+/* The monotor command.*/
 #define MONITOR (char* const[]){"udevadm",\
 	                        "monitor",\
 	                        "--udev",\
 	                        "-s",\
                           	"block",\
         	                NULL}
-#define MAX_FD 8192
-#define STDIN 0
-#define STDOUT 1
-#define STDERR 2
-#define LINE_LENGTH 250
-#define PATH_LENGTH 150
-#define ADD "add"
-#define ADD_NUM 3
-#define IN 0
-#define OUT 1
+#define MAX_FD 8192       /* Random number for max open fds.*/
+#define STDIN 0           /* The stdin fd.*/ 
+#define STDOUT 1          /* The stdout fd.*/ 
+#define STDERR 2          /* The stderror fd.*/ 
+#define LINE_LENGTH 250   /* Max length of monitor output.*/
+#define PATH_LENGTH 150   /* Max length of device path.*/
+#define ADD "add"         /* Add word for comparison.*/
+#define ADD_NUM 3         /* The position of the add word in the output.*/
+#define IN 0              /* Flag for declaring that the parser is in a word.*/
+#define OUT 1             /* Flag for declaring that the parser out of a word.*/
 
 #endif
